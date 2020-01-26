5297ef1ba559b1061e997abedf05cccbaa1cc896
Vladimir Kozhevnikov <44900317+UnrealDrunk@users.noreply.github.com>
2020-01-26 18:33:06 +0300

    Create SmallLazerLScript.cs

---
+++ SmallLazerLScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallLazerLScript : MonoBehaviour
+{
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(-1, 0, 1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
